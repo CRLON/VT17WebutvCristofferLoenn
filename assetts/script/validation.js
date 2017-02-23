9b3d7bd7d4cf564756078971c709fc37e558d189
CRLON <cristofferlonn@gmail.com>
2017-02-23 23:25:45 +0100

    minor code cleanup

--- assetts/script/validation.js
+++ assetts/script/validation.js
@@ -2,45 +2,43 @@
 
 var idInput = document.getElementById('idNumber');
 idInput.addEventListener('input', checkIdNumber);
-var idMark = document.getElementById('idNumberResult')
-idMark.innerHTML = "\u2012"
+var idMark = document.getElementById('idNumberResult');
+idMark.innerHTML = "\u2718";
 
 function checkIdNumber() {
-  var tempInt = 0;
-  var anotherTempInt = 0;
+  var inputInt = 0;
   var idSum = 0;
 
   if(idInput.value != 10 || idInput.value != 12) {
-    idMark.innerHTML = "\u2012"
+    idMark.innerHTML = "\u2718";
   }
 
-  if(idInput.value.length === 12) {
-    anotherTempInt = idInput.value;
-    idInput.value = anotherTempInt.substring(2, 12);
+  if(idInput.value.length == 12) {
+    idInput.value = idInput.value.substring(2, 12);
   }
 
-  if(idInput.value.length === 10) {
+  if(idInput.value.length == 10) {
     for(var i = 0; i < 10; i++) {
-         tempInt = parseInt(idInput.value[i]);
-      if(i % 2 === 0) {
-        tempInt *= 2;
-        if(tempInt > 9) {
-          tempInt -= 9;
+         inputInt = parseInt(idInput.value[i]);
+      if(i % 2 == 0) {
+        inputInt *= 2;
+        if(inputInt > 9) {
+          inputInt -= 9;
         }
-        idSum += tempInt;
+        idSum += inputInt;
       }
       else {
-        idSum += tempInt;
+        idSum += inputInt;
       }
     }
-     if(idSum % 10 === 0) {
+     if(idSum % 10 == 0) {
       idMark.innerHTML = "\u2714";
      }
      else {
-      idMark.innerHTML = "\u2718"
+      idMark.innerHTML = "\u2718";
      }
   }
-};
+}
 
 var leapYearInput = document.getElementById('leapYear');
 leapYearInput.addEventListener('input', checkLeapYear);
@@ -50,13 +48,13 @@ leapMark.innerHTML = "\u2012";
 function checkLeapYear() {
   var leapYearInt = parseInt(leapYearInput.value, 10);
 
-  if(leapYearInt % 4 === 0 && leapYearInt % 100 != 0 || leapYearInt % 400 === 0) {
+  if(leapYearInt % 4 == 0 && leapYearInt % 100 != 0 || leapYearInt % 400 == 0) {
     leapMark.innerHTML = "\u2714";
   }
   else {
     leapMark.innerHTML = "\u2718";
   }
-};
+}
 
 var numberSumInput = document.getElementById('numberSum');
 numberSumInput.addEventListener('input', numberSumCalc);
@@ -69,4 +67,4 @@ function numberSumCalc() {
     returnSum += parseInt(numberSumInput.value[i], 10);
   }
   numberSumResult.innerHTML = returnSum;
-};
+}
