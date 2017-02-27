279c6857cc835ff8f9cab56795d40bd6a13d0924
CRLON <cristofferlonn@gmail.com>
2017-02-27 22:33:00 +0100

    minor css & javascript changes

--- assetts/script/slutuppgift.js
+++ assetts/script/slutuppgift.js
@@ -1,68 +1,78 @@
 "use strict"
 //API-Nyckel: 7bfcd321e8203f59
 
-var userInput = document.getElementById('numberInput');
-userInput.addEventListener('input', inputModifier)
-var ingredientAmountList = document.getElementsByClassName('ingredients__amount');
-var currentAmount = parseFloat(document.getElementById('numberInput').value);
-var stars = document.getElementsByClassName('stars');
+var userInput = document.getElementById("numberInput");
+userInput.addEventListener("input", inputModifier)
+var ingredientAmountList = document.getElementsByClassName("ingredients__amount");
+var currentAmount = parseFloat(document.getElementById("numberInput").value);
+var stars = document.getElementsByClassName("stars");
 var starValue = 5;
-var loadingAnimation = document.getElementById('loading__gif');
-var numberOfBatches = localStorage.getItem('numberOfBatches');
-//window.onloadend = setLocalStorage();
+var loadingAnimation = document.getElementById("loading__gif");
+var numberOfBatches = localStorage.getItem("numberOfBatches");
+var xhttp = new XMLHttpRequest();
+var response;
+var hasVoted;
 
-//function setLocalStorage() {
-if(localStorage.getItem('numberOfBatches') != 'null') {
-	userInput.value = localStorage.getItem('numberOfBatches');
+if(localStorage.getItem("numberOfBatches") == "null") {
+    localStorage.setItem("numberOfBatches", "1");
 	}
 	else {
-		localStorage.setItem('numberOfBatches', '1');
+    userInput.value = localStorage.getItem("numberOfBatches");
 	}
-inputModifier();
 
-
+if(localStorage.getItem("hasVoted") == "null") {
+    localStorage.setItem("hasVoted", "true");
+  }
+  else {
+    var hasVoted = localStorage.getItem("hasVoted")
+  }
 
 function inputModifier() {
-  var newAmount = document.getElementById('numberInput').value;
+  var newAmount = document.getElementById("numberInput").value;
   for(var i = 0; i < ingredientAmountList.length; i++) {
     var temp = parseFloat(ingredientAmountList[i].innerText, 10);
     ingredientAmountList[i].innerText = (temp / currentAmount) * newAmount;
-  }
-  currentAmount = parseFloat(document.getElementById('numberInput').value);
-  localStorage.setItem('numberOfBatches', currentAmount);
+    }
+  currentAmount = parseFloat(document.getElementById("numberInput").value);
+  localStorage.setItem("numberOfBatches", currentAmount);
 }
+inputModifier();
 
 for(var i = 0; i < stars.length; i++) {
-	stars[i].addEventListener('click', addVote);
+	stars[i].addEventListener("click", addVote);
 	stars[i].value = starValue;
 	starValue--;
 }
 
 function fillStars (value) {
-
 	var starNumber = 4;
-
 	for (var i = 0; i < value; i++) {
 		stars[starNumber].innerHTML = "&starf;";
 		starNumber--;
 	}
 }
 
+function getRating() {
+  showLoadingAnimation();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      response = JSON.parse(this.responseText);
+  xhttp.open("GET", "https://edu.oscarb.se/sjk15/api/recipe/?api_key=7bfcd321e8203f59&recipe=blåbärspaj", true);
+   }
+  }
+}
+getRating();
 
-function addVote() {
-
-	showLoadAnimation();
-	console.log(loadingAnimation.style.visibility)
-	fillStars(this.value);
-	console.log(this.value);
-	loadingAnimation.style.visibility = 'hidden';
+function showLoadingAnimation() {
+  loadingAnimation.style.visibility = "visible";
 }
 
-function showLoadAnimation() {
-	if (loadingAnimation.style.visibility == 'hidden') {
-		loadingAnimation.style.visibility = 'visible';
-	}
-	else if (loadingAnimation.style.visibility == 'visible') {
-		loadingAnimation.style.visibility = 'hidden';
-	}
+function hideLoadingAnimation() {
+  loadingAnimation.style.visibility = "hidden";
+}
+
+function addVote() {
+  if(hasVoted == "false") {
+    fillStars(this.value);
+  }
 }
